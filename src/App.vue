a50300dd4357fb66772e25dd34934c2dcabf3906
Alexander Zavialov <alexanderverone@gmail.com>
2023-04-12 12:17:31 +0300

    add routes transitions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
     <TheHeader />
   </header>
   <section class="main">
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </transition>
+    </RouterView>
   </section>
 </template>
 
@@ -20,4 +27,14 @@ onMounted(() => {
 })
 </script>
 
-<style scoped></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
