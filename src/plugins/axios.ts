afae42ac7ec6c060f97e9dd69bdc638e79ad43e2
Alexander Zavialov <alexanderverone@gmail.com>
2023-04-19 11:26:02 +0300

    replace static baseUrl for axios instance by environment variable

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_ENDPOINT_HOST,
 })
 
 axiosInstance.interceptors.request.use((config) => {
